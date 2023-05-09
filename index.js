bc1c03b6498472e401fba4be9062daa177c39cd5
Chanda Hopkins <chandahopkins@outlook.com>
2023-05-08 22:01:02 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    if (pickUp > 42) {
+        return (pickUp - 42)
+    } else {
+        return (42 - pickUp)
+    }
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp) * 264
+}
+
+function distanceTravelledInFeet(pickUp, dropOff) {
+    if (pickUp > dropOff) {
+        return (pickUp - dropOff) * 264
+    } else {
+        return (dropOff - pickUp) * 264
+    }
+}
+
+function calculatesFarePrice(pickUp, dropOff) {
+    let distance = distanceTravelledInFeet(pickUp, dropOff)
+    if (distance <= 400) {
+        return distance * 0
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * .02 
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+
+}
